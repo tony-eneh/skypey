5d3f0efe6f83c08186032d56da50a34fbcce111e
Tony Eneh <aeneh90@gmail.com>
2020-10-27 15:31:33 +0100

    Feat: style the display of message times

--- src/Components/Chat.js
+++ src/Components/Chat.js
@@ -3,7 +3,8 @@ import "./Chat.css";
 export const Chat = (props) => {
   return (
     <span className="chat" {...props}>
-      Hello Niggi
+      <span className="chat__text">Hello Niggi</span>
+      <span className="chat__time">03:15pm</span>
     </span>
   );
 };
