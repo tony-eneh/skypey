1041fe50ae780408eefd3a1698ce280fbaf23e55
Tony Eneh <aeneh90@gmail.com>
2020-10-28 17:10:14 +0100

    Feat: display dynamic user data in header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,17 +1,15 @@
 import "./Header.css";
+import { store } from "../store";
 
 export const Header = () => {
+  // header area currently displays details of active user which can be deceiving
+  // TODO: use header to display info of current chat partner or group chat info(for group chats)
+  const { name, profilePic, status } = store.getState().activeUser;
   return (
     <header className="header">
-      <img
-        className="header__img"
-        src="https://picsum.photos/200"
-        alt="Ada Idah"
-      />
-      <h1 className="header__name">Ada Idah</h1>
-      <p className="header__status">
-        lorem ipsum donect sur ametis ina tuin cahala suis ejus
-      </p>
+      <img className="header__img" src={profilePic} alt={name} />
+      <h1 className="header__name">{name}</h1>
+      <p className="header__status">{status}</p>
     </header>
   );
 };
