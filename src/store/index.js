fbdb7414a007120459d2126ec4298a345843d3b1
Tony Eneh <aeneh90@gmail.com>
2020-10-28 15:37:16 +0100

    chore: initialize redux store with demo data

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,6 @@
+import { initial } from "lodash";
 import { createStore } from "redux";
 import { reducer } from "../reducers";
+import { state } from "../static-data";
 
-export const store = createStore(reducer);
+export const store = createStore(reducer, state);
