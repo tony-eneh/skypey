fd02f2dc20960eab46c99d9cc74cb85fab1c4908
Tony Eneh <aeneh90@gmail.com>
2020-10-30 16:20:42 +0100

    Fix: missing input area on load for mobile devices

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import "./App.css";
 import { Sidebar } from "./Components/Sidebar";
 import { ChatWindow } from "./Components/ChatWindow";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    const handleResize = () => {
+      // create a custom property a.k.a css variable to hold the current viewport height
+      document.documentElement.style.setProperty(
+        "--vh100",
+        `${window.innerHeight}px`
+      );
+    };
+
+    // add resize event listener
+    window.addEventListener("resize", handleResize);
+    window.addEventListener("load", handleResize);
+  });
+
   return (
     <div className="app">
       <Sidebar />
