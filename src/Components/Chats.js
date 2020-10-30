1f2a1e64da2dadd62849d385a540f9684aafaac7
Tony Eneh <aeneh90@gmail.com>
2020-10-30 13:19:19 +0100

    Feat: scroll to bottom of chats on new message

--- src/Components/Chats.js
+++ src/Components/Chats.js
@@ -1,11 +1,20 @@
 import { Chat } from "./Chat";
 import "./Chats.css";
 import { store } from "../store";
+import { useEffect, createRef } from "react";
 
 // there is a little in the meaning of Chats in my UI and in the redux store
 // Chats in my UI represents the discussion (chat) with one of my contacts. Hence a Chat in my UI is actually a message in the redux store
 
 export const Chats = () => {
+  // create a ref to identify this component's dom element for scrolling purpose
+  const chatsRef = createRef();
+
+  // scroll chats down on component mount or update
+  useEffect(() => {
+    chatsRef.current.scrollTop = chatsRef.current.scrollHeight;
+  });
+
   const state = store.getState();
   const currentChat = state.chats.find(
     (chat) => chat.chatId === state.currentChatId
@@ -15,7 +24,7 @@ export const Chats = () => {
   console.log("chat members", currentChat.members);
   console.log("messages", messages);
   return (
-    <div className="chats">
+    <div className="chats" ref={chatsRef}>
       {messages.map((message) =>
         message.sender === activeUser.userId ? (
           <Chat chat={message} key={message.messageId} data-user-message />
